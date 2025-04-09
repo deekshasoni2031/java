ce32a8c5fb1e6030736149300f0fd9a003a0500f
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-04-09 21:04:22 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,10 @@
+public class operators{
+    public static void main(String[] args){
+        int a=3;
+        int b=5;
+        System.out.println("addition:" +(a+b));
+        System.out.println("subtraction:"+(a-b));
+        System.out.println("multiplication:"+(a*b));
+        System.out.println("division:"+(a/b));
+    }
+}
