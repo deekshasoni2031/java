5bb9aca4f467280903a3d844f6bb5d50fee3438f
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-04-11 10:05:15 +0530

    Add files via upload

---
+++ switch_case.java
@@ -0,0 +1,19 @@
+public class switch_case {
+    public static void main(String[] args) {
+        int a=5;
+        switch (a) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            System.out.println("hello");
+            break;
+            case 5:
+            System.out.println("hiiiii");
+            break;
+            default :
+
+        }
+    }
+    
+}
