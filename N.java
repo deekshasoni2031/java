270ba306422cf4e8253297c11a756fc7e8f6f5e1
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-04-15 13:46:54 +0530

    Add files via upload

---
+++ N.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class N{
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner (System.in);
+        System.out.println("enter n:");
+        int n= in.nextInt();
+
+        for(int i=0;i<n;i++){
+            System.out.println(i);
+
+
+        }
+
+    }
+}
