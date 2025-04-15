8a3133e07eb5afd8ea08f65fe95faf4288daaf4d
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-04-15 12:30:10 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,18 @@
+
+public class whileloop {
+    public static void main(String[] args) {
+        //for (int i=0;i<5;i++){
+        //System.out.println("hello");
+        //System.out.println(i*2);
+        //}
+
+        int a=2;
+        while(a<5){
+            System.out.println("hello!!!!!!");
+            a++;
+        }
+        
+        
+    }
+    
+}
