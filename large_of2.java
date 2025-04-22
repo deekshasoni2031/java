e93f5d1bff4c85371e6c97c5c4f3e88233145e9f
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-04-22 23:07:41 +0530

    Add files via upload

---
+++ large_of2.java
@@ -0,0 +1,10 @@
+public class large_of2 {
+    public static void main(String[] args) {
+        int a=34 ,b=10;
+        if(a>b)
+        System.out.println("a is greater");
+        else
+        System.out.println("b is grater");
+    }
+    
+}
