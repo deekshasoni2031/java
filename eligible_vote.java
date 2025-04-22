9c63815d2cc19f6202e0cd435d2d7fceb74fa1e0
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-04-22 23:05:59 +0530

    Add files via upload

---
+++ eligible_vote.java
@@ -0,0 +1,10 @@
+public class eligible_vote {
+    public static void main(String[] args) {
+        int age=10;
+        if (age>=18)
+          System.out.println("is eligible");
+        else
+        System.out.println("not eligible");  
+    }
+    
+}
