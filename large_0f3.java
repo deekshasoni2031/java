1ae51d842d3c8c5579c3d41775774c964f05e46d
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-04-22 23:07:18 +0530

    Add files via upload

---
+++ large_0f3.java
@@ -0,0 +1,14 @@
+public class large_0f3 {
+    public static void main(String[] args) {
+        int a=10 ,b=34 ,c=2;
+        if(a>c && a>b){
+            System.out.println("a is the greater no.");
+        }
+        else if(b>c){
+            System.out.println("b is the greater no.");
+        }
+        else
+        System.out.println("c is the greater no.");
+    }
+    
+}
