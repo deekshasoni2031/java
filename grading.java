5d286b1f8b8fd8b7f7a34e2108b97abe9ec2ecc2
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-04-22 23:06:57 +0530

    Add files via upload

---
+++ grading.java
@@ -0,0 +1,14 @@
+public class grading {
+    public static void main(String[] args) {
+        int marks=89;
+        if (marks>=90)
+        System.out.println("grade A");
+        else if(marks>=75)
+        System.out.println("grade B");
+        else if(marks>=60)
+        System.out.println("grade C");
+        else
+        System.out.println("fail");
+    }
+    
+}
