703e0bb5c9220b0900b890ef01864f6352ca73d4
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-04-23 23:04:17 +0530

    Add files via upload

--- whileloop.java
+++ whileloop.java
@@ -1,13 +1,13 @@
 
 public class whileloop {
     public static void main(String[] args) {
-        //for (int i=0;i<5;i++){
+        /*for (int i=1;i<11;i++){
         //System.out.println("hello");
-        //System.out.println(i*2);
-        //}
+        System.out.println(i*5);
+        }*/
 
-        int a=2;
-        while(a<5){
+        int a=1;
+        while(a<6){
             System.out.println("hello!!!!!!");
             a++;
         }
