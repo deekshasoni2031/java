49418aa571031a1098cc3e58050094065da6aa60
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-04-23 23:03:40 +0530

    Add files via upload

---
+++ one_to_n.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class one_to_n {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("entr the value of n: ");
+        int n= in.nextInt();
+
+        for(int i=0;i<n;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
