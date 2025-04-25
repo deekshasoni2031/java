cc95d7c1d244c92a6effb7637d518dee2834b802
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-04-25 13:01:26 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,9 @@
+public class variables {
+    static int a=20;  //static using static variable
+    //int a=10; //non-static using instant variables
+    public static void main(String[] args) {
+       // variables obj= new variables(); 
+        //System.out.println(obj.a);
+        System.out.println(variables.a);
+    }
+}
