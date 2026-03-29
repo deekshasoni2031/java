24b6dd12b424ac64e35901ddcd56c2b6f3452b26
deekshasoni2031 <deekshasoni2031@gmail.com>
2026-03-29 10:56:38 +0530

    Add files via upload

--- variables.java
+++ variables.java
@@ -1,9 +1,27 @@
-public class variables {
-    static int a=20;  //static using static variable
-    //int a=10; //non-static using instant variables
-    public static void main(String[] args) {
-       // variables obj= new variables(); 
-        //System.out.println(obj.a);
-        System.out.println(variables.a);
-    }
+public class variables{
+    //instance variable(belongs to each object)
+    int instanceVariable = 10 ;
+
+    //static variable(shared across all objects of the class)
+     static String staticVar = "i am static" ;
+     
+     //local variable
+     public void showVariables(){
+        //local variable(declared inside a method)
+        int localVar = 5 ;
+
+        System.out.println("instance variable: "+instanceVariable );
+        System.out.println("static variable: "+staticVar);
+        System.out.println("local variable: "+localVar);
+
+
+     }
+     public static void main(String[] args) {
+         //creating object
+         variables obj1 = new variables();
+         obj1.showVariables();
+
+         //accessing static variable directly using class name
+         System.out.println("accessing static variable via class: "+variables.staticVar);
+     }
 }
